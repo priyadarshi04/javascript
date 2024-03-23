20e0db48730ac129c9b8843a434f6ff631d595bb
Priyadarshi Rajeev <115398852+priyadarshi04@users.noreply.github.com>
2024-03-23 20:16:56 +0000

    done function

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,12 @@
+let a =10
+const b =20
+con
+console.log("INNER: ", a);
+}
+
+for (let i=0; i< Array.length;i++){
+    const element = array[i];
+}
+console.log(a);
+//console.log(b);
+//console.log(c);
