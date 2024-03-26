02ef40d9809ed394eb728a6190bdd8c1aeccd201
Priyadarshi Rajeev <115398852+priyadarshi04@users.noreply.github.com>
2024-03-26 07:05:12 +0000

    continue

---
+++ 04_controlflow/one.js
@@ -0,0 +1,28 @@
+// if
+// const isUserloggedIn =true 
+// const temperature = 41
+
+// if( temperature === 40){
+//     console.log("less than 40");
+
+// }else{
+// console.log("temperature is greater than 40");
+// }
+
+// console.log("Executed"); 
+// const score = 200 
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+const balance =1000
+
+//if(balance >500) console.log("test"),console.log("test2")
+                                    // we don't have to do the above practice of line no 25
+
+
