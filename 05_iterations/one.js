b05796181226b324713fb94cb45a638935fde449
Priyadarshi Rajeev <115398852+priyadarshi04@users.noreply.github.com>
2024-03-27 22:07:14 +0000

    loop done

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,5 +1,45 @@
 //for
+for( let i = 0 ; i <= 10; i++){
+  const element = i;
+  if (element == 5) {
+  //console.log( "5 is best number");
+  }
+  //console.log(element);
 
-for( let index = 0 ; index < Array.length; index++){
-  const element = array[index];
+}
+//console.log(element);
+
+// for( let i = 1 ; i <= 10; i++){
+//   console.log(`Outer loop value : ${i}`);
+//   for( let j = 1 ; j <= 10 ; j++){
+//     //console.log(`Inner loop value ${i} and inner loop ${j}`);
+//     console.log(i+ "*" +j + ' = ' + i * j );
+//   }
+
+// }
+
+let myArray = ["flask" , "batm" ,"supermen"]
+  //  console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  // console.log(element);
+  
+}
+
+//break and condition
+
+// for (let index = 0; index <= 20 ; index++) {
+//   if(index == 5){
+//     console.log(`Detected 5`);
+//   }
+//   console.log(`value of i is ${index}`);
+  
+// }
+for (let index = 0; index <= 20 ; index++) {
+  if(index == 5){
+    console.log(`Detected 5`);
+    continue
+  }
+  console.log(`value of i is ${index}`);
+  
 }
