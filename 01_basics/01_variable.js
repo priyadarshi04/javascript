8edfa5fc4d2163211f3d9469fe2c1f1d6e446f85
Priyadarshi Rajeev <115398852+priyadarshi04@users.noreply.github.com>
2024-03-31 21:14:13 +0000

    committed

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,20 +1,30 @@
-const accountId = 1445
-let accountEmail = "rajeev.11aug@gmail.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
-let accountState ;
+// const accountId = 1445
+// let accountEmail = "rajeev.11aug@gmail.com"
+// var accountPassword = "12345"
+// accountCity = "Jaipur"
+// let accountState ;
 
-// accountId = 2 //not allowed
+// // accountId = 2 //not allowed
 
-accountEmail = "rajeevpoddar@gmail.com"
-accountPassword = "21221212"
-accountCity = "Bengaluru"
+// accountEmail = "rajeevpoddar@gmail.com"
+// accountPassword = "21221212"
+// accountCity = "Bengaluru"
 
-console.log(accountId);
+// console.log(accountId);
 
 /*
 Prefer not to use var because of 
 issue in block scope and functional scope
 
 */
-console.table([accountId,accountEmail, accountPassword,accountCity,accountState])
+//console.table([accountId,accountEmail, accountPassword,accountCity,accountState])
+
+console.log("har\"".length);
+
+let numbers = [1,2,4,5,6]
+//numbers[1] = 8;
+//numbers.shift()
+numbers.unshift()
+//numbers.push(7)
+//numbers.pop();
+console.log(numbers);
